a5467d6eef3767f8968bfef3a734d62fcc7997f7
Tony Bowden <tony@tmtm.com>
2017-01-13 13:29:15 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'date'
 require 'json'
@@ -14,31 +15,28 @@ end
 added = Hash.new(0)
 json = json_from('https://raw.githubusercontent.com/centraldedados/parlamento-deputados/master/data/deputados.json')
 json.values.each do |v|
-  mp_data = { 
-    id: v[:id],
+  mp_data = {
+    id:                     v[:id],
     identifier__parlamento: v[:id],
-    name: v[:shortname],
-    full_name: v[:name],
-    birth_date: v[:birthdate],
-    image: v[:image_url],
-    source: v[:url],
+    name:                   v[:shortname],
+    full_name:              v[:name],
+    birth_date:             v[:birthdate],
+    image:                  v[:image_url],
+    source:                 v[:url],
   }
 
   v[:mandates].each do |m|
     term_data = {
-      term: m[:legislature],
-      party: m[:party],
-      area: m[:constituency],
+      term:       m[:legislature],
+      party:      m[:party],
+      area:       m[:constituency],
       start_date: m[:start_date],
-      end_date: m[:end_date],
+      end_date:   m[:end_date],
     }
 
     data = mp_data.merge term_data
-    ScraperWiki.save_sqlite([:id, :term], data)
+    ScraperWiki.save_sqlite(%i(id term), data)
     added[term_data[:term]] += 1
   end
-
 end
 puts "  Added #{added}"
-
-
